48f04a6a457885726bf2782c4c358de77b06b739
pedrod <darivafilho@hotmail.com>
2018-09-28 20:07:25 -0300

    adicao de css aprimorado

--- index.php
+++ index.php
@@ -21,6 +21,6 @@
 					<input type="password" name="pwd" placeholder="Password">
 					<button type="submit" name="submit">Login</button>
 				</form>
-				<a href="signup.php">Sign up</a>'
+				<a href="signup.php">Sign up</a>
 </body>
 </html>
