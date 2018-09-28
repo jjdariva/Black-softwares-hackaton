d4207ae9a58c90a01870fa771bace31a19793c00
pedrod <darivafilho@hotmail.com>
2018-09-28 19:28:35 -0300

    começando a pagina home

--- index.php
+++ index.php
@@ -1,44 +1,20 @@
+<?php  
+	session_start()
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
 	<title></title>
-		<style>
-   		table{
-   			table-layout: auto;
-   			border: 2px solid black;
-   		}
-   		table.a{
-   			table-layout: auto;
-    		width: 700px; 
-    		height: 500px;   
-   		}
-   		tr,td {
-    		border: 1px solid black;
-    		font-size: 20px;
-		}
-		.input{
-			height: 40px;
-		}
-   	</style>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<div>
-		<center><p><h2>Mortalidade</h2></p></center>
-		<table class="a">
-			  <tr>
-			    <th>Mortalidade Diária</th>
-			    <th>1°</th>
-			  </tr>
-			  <tr>
-			    <td>Morte Natural</td>
-			    <td><input class="input" type="number"></td>		    
-			  </tr>
-			  <tr>
-			    <td>Eliminado</td>
-			    <td><input class="input" type="number"></td>
-			  </tr>
-		</table>
-	</div>
+<header>
+	<nav>
+		<div class="main-wrapper">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+			</ul>
+			<div class="nav-login">
 </body>
 </html>
