8addf0169191a7f3d558ac904edcdc5c66c967b9
João Danilo <jddiedrich@gmail.com>
2018-09-28 19:10:58 -0300

    'v2'

--- index.php
+++ index.php
@@ -3,8 +3,42 @@
 <head>
 	<meta charset="utf-8">
 	<title></title>
+		<style>
+   		table{
+   			table-layout: auto;
+   			border: 2px solid black;
+   		}
+   		table.a{
+   			table-layout: auto;
+    		width: 700px; 
+    		height: 500px;   
+   		}
+   		tr,td {
+    		border: 1px solid black;
+    		font-size: 20px;
+		}
+		.input{
+			height: 40px;
+		}
+   	</style>
 </head>
 <body>
-
+	<div>
+		<center><p><h2>Mortalidade</h2></p></center>
+		<table class="a">
+			  <tr>
+			    <th>Mortalidade Diária</th>
+			    <th>1°</th>
+			  </tr>
+			  <tr>
+			    <td>Morte Natural</td>
+			    <td><input class="input" type="number"></td>		    
+			  </tr>
+			  <tr>
+			    <td>Eliminado</td>
+			    <td><input class="input" type="number"></td>
+			  </tr>
+		</table>
+	</div>
 </body>
 </html>
