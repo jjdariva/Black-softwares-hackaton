16227d5dc15fb084f19d0419ded4e4e7868f46c8
João Danilo <jddiedrich@gmail.com>
2018-09-28 19:59:10 -0300

    mudei css

---
+++ mortalidadepagina.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div>
+		<center><h2>Mortalidade</h2></center>
+		<table class="a">
+			  <tr class="coluna">
+			    <th>Mortalidade Diária</th>
+			    <th>1° Dia</th>
+			  </tr>
+			  <tr>
+			    <td>Morte Natural</td>
+			    <td><input class="input" type="number" placeholder="ex:15"></td>		    
+			  </tr>
+			  <tr>
+			    <td>Eliminado</td>
+			    <td><input class="input" type="number" placeholder="ex:50"></td>
+			  </tr>
+		</table>
+	</div>
+</body>
+</html>
