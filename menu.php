072b95698cb49a001242490fabdb7170a13167cc
jonas saldanha <coxao023@gmail.com>
2018-09-28 23:15:43 -0300

    menu do site

---
+++ menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+		.fundo{
+			background-color: #CCC;
+		}
+		.divisao{
+			background-color: #C0C0C0;
+			height: 900px;
+			width: 400px;
+		}
+		.font{
+			font-family: arial;
+			font-size: 45px;
+		}
+	</style>
+	<title>ABATE CHIQ</title>
+</head>
+<body class="fundo">
+	<div class="divisao">
+		<p></p>
+	</div>
+</body>
+</html>
